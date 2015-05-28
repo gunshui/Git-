50b91a852d1ef4c8504298db59168c58dff1c2f2
digime <digime@digime-iMac.AirDream>
2015-05-28 10:50:52 +0800

    Initial Commit

---
+++ Git工程测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git工程测试
+//
+//  Created by digime on 15/5/28.
+//  Copyright (c) 2015年 digime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git工程测试/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git工程测试
+//
+//  Created by digime on 15/5/28.
+//  Copyright (c) 2015年 digime. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
